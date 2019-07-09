25f4b2666b4be83f3aca4eb8e0a9f047c579f05f
jtx007 <jamesjacobthomas7@gmail.com>
2019-07-09 00:52:40 -0400

    updated yard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::API
     include ExceptionHandler
 
-    def authorize
-        puts 'hello'
-        authorize =  Authorization.new(request)
-        
-        puts authorize.current_user
-    end
+    
 end
